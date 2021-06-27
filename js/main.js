f4f7a975a1f7766d7615eb3bff9e290f4f55c367
TeyaLir <ia_88@bk.ru>
2021-06-27 18:24:20 +0300

    Add header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
+  $(document).ready(function() {
+    $('.menu__btn').click(function(event) {
+      $('.menu__btn, .menu').toggleClass('active');
+      $('body').toggleClass('lock');
+    });
+  });
+
 });
+
