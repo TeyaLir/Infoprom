60e0a3642bda4bf812df76123a788333bfb9e661
TeyaLir <ia_88@bk.ru>
2021-07-21 22:21:50 +0300

    Add animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Анимация движения блоков при скролле
+AOS.init();
+
 $(function () {
 
   // Бургер меню
@@ -141,7 +144,6 @@ $(function () {
           slidesToShow: 1,
         }
       },
-
     ]
   });
 
@@ -151,4 +153,30 @@ $(function () {
 $('[data-fancybox]').fancybox({
   loop: true,
 });
+
+// Анимация цифр в секции numbers
+$(document).ready(function () { 
+  var show = true;
+  var countbox = ".numbers__inner";
+  $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      var w_height = $(window).height(); // Высота окна браузера
+      var d_height = $(document).height(); // Высота всего документа
+      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      if (w_top + 800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+          $('.numbers__box-num').css('opacity', '1');
+          $('.numbers__box-num').spincrement({
+              thousandSeparator: " ",
+              duration: 3000
+          });
+           
+          show = false;
+      }
+  });
+});
+
+
+
 
